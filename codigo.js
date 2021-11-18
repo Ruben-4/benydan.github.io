17f15ce3751cd544101db58588b394c5f2719014
benydan <ruben.ramosg@estudiante.uam.es>
2021-11-18 16:17:02 +0100

    stilos ssc

--- codigo.js
+++ codigo.js
@@ -3,5 +3,17 @@ function addItem(){
     const tipo = document.getElementById("options").value
     console.log(cantidad)
     console.log(tipo)
+
+    const fila = document.createElement("tr")
+    const c1 = document.createElement("td")
+    c1.innerText = cantidad
+    const c2 = document.createElement("td")
+    c2.innerText = tipo
+    fila.appendChild(c1)
+    fila.appendChild(c2)
+
+    const items = document.getElementById("items")
+    items.appendChild(fila)
+
 }
     
